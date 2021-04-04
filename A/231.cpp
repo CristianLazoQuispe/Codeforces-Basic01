11643c67224c3bfd360b15ebd425d145f5b27ba2
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2021-04-03 23:40:56 -0500

    ok

---
+++ A/231.cpp
@@ -0,0 +1,15 @@
+// 231 A. Team
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,a,b,c,ans=0;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        ans+=(a+b+c)/2;
+    }
+    cout<<ans;
+
+}
